996db7724d835654215ffd5b0000e5fa05a0e2a5
Timo Lindemann <coffeeprocessor@gmail.com>
2018-02-22 17:51:02 +0100

    little fixes

--- github.com/github-ui.js
+++ github.com/github-ui.js
@@ -11,41 +11,43 @@
 
 (function() {
   /**
-    * If GH ever changes its UI so the selectors no longer work, throw
-    * an exception stating this clearly.
-    * 
-    * @param {string} selector A jQuery-Selector
-    * @return {jQuery} the jquery object.
-    */
+   * If GH ever changes its UI so the selectors no longer work, throw
+   * an exception stating this clearly.
+   *
+   * @param {string} selector A jQuery-Selector
+   * @return {jQuery} the jquery object.
+   */
   function $$$(selector) {
     const sel = $(selector);
     if (sel.length === 0) {
-      throw new Error('GH-TM-UI: Selector did not work; stopping script');
+      throw new Error("GH-TM-UI: Selector did not work; stopping script");
     } else {
       return sel;
     }
   }
 
-  // we need the user name; get it from the "your profile" link
-  const username = $$$('a.dropdown-item:contains(Your profile)')
-    .attr('href')
+  // we need the username; get it from the "your profile" link
+  const username = $$$("a.dropdown-item:contains(Your profile)")
+    .attr("href")
     .slice(1);
 
   /**
    * inserts a link to the "My repositories"-page into the main navigation.
+   *
+   * @method insertRepoLink
    */
   function insertRepoLink() {
-    const repolink = 'https://github.com/' + username + '?tab=repositories';
-    const ui = $$$(
+    const repolink = "https://github.com/" + username + "?tab=repositories";
+    const ui = $(
       `<li><a class="HeaderNavlink px-2" href="${repolink}"> Repos </li>`
     );
-    if ($) {
-      $('ul.d-flex[role="navigation"]').prepend(ui);
-    }
+    $$$('ul.d-flex[role="navigation"]').prepend(ui);
   }
 
   /**
    * Hides the "Marketplace" and "Explore" links from the main navigation.
+   *
+   * @method removeUnusedNavigation
    */
   function removeUnusedNavigation() {
     // the "Marketplace" Link
